63b7c74f776d78a8e35594f707792e64a54e9496
private09773 <tempt0576@gmail.com>
2026-07-26 08:28:22 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# bangkok-bot
-Bot for Bangkok Airways PTFS.
+# Bangkok Airways Bot
+---
+
+> This bot was made for Bangkok Airways PTFS which is owned by t67wwp.
+
+> This bot is designed for Flight Planning, Announcements and games.
+
+**Quick Links**
+1. [Install The Bot](INSTALLATION.md)
+2. [Check The Readme](README.md)
+3. [Check the License](LICENSE)
+
+**Version**
+**This application is developed using Python and Discord.Py**
+![Discord.py](https://shields.io/version="2.4.2")
---
+++ main.py
@@ -0,0 +1,63 @@
+import os
+import discord
+import asyncio
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Configuring Gateway Intents.
+intents = discord.Intents.default()
+intents.message_content = True  # Required for Prefix Commands.
+intents.members = True  # Privileged Intent for Member Events.
+
+# Instiantiate Bot Client with prefix, default prefix is "." but you can change it to your preference.
+bot = commands.Bot(command_prefix=".", intents=intents)
+
+# Load all cogs (modules).
+initial_extensions = [
+    "cogs.utils",
+    "cogs.announcement"
+]
+
+@bot.event
+async def setup_hook():
+    for extension in initial_extensions:
+        await bot.load_extension(extension)
+        
+# Running The Bot
+@bot.event
+async def on_ready():
+    print(f"Online as {bot.user.name}.")
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user.name}!")
+
+    # Set presence INSIDE the async function
+    activity = discord.Activity(
+        type=discord.ActivityType.watching, 
+        name="in development"
+    )
+    await bot.change_presence(status=discord.Status.dnd, activity=activity)
+    
+if __name__ == "__main__":
+    TOKEN = os.getenv("DISCORD_TOKEN")
+    if TOKEN:
+        bot.run(TOKEN)
+    else:
+        print("Error: You have not placed your token inside the '.env' file, place it inside there.")
+        
+if __name__ == "__main__":
+    GUILD = os.getenv("GUILD_ID")
+    if GUILD:
+        print("Your Guild ID is succesfully Configured, the bot will run.")
+    else:
+        print("Error: Your Guild ID is not set, please set it in the '.env' file.")
+        
+if __name__ == "__main__":
+    OWNER = os.getenv("OWNER_ID")
+    if OWNER:
+        print("Your OWNER ID is succesfully configured, the bot will run.")
+    else:
+        print("Error: Owner ID is not setted up.")
